3663948ee4f2ae628dfa77cfa490138deed469d9
ghalyahk <ghalyah1166@gmail.com>
2025-10-30 15:57:49 +0000

    Fix Big O file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,50 +2,44 @@
 #include <stdio.h>
 
 /**
- * swap_nodes - Swap two nodes in a doubly linked list
- * @list: Pointer to the head of the list
- * @node1: Node to swap with previous
- * @node2: Node to swap with next
- */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
-{
-    if (node1->prev)
-        node1->prev->next = node2;
-    else
-        *list = node2;
-
-    if (node2->next)
-        node2->next->prev = node1;
-
-    node1->next = node2->next;
-    node2->prev = node1->prev;
-
-    node2->next = node1;
-    node1->prev = node2;
-}
-
-/**
  * insertion_sort_list - Sorts a doubly linked list of integers
- *                       in ascending order using Insertion Sort
- * @list: Pointer to the head of the list
+ *                       in ascending order using insertion sort
+ * @list: Double pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *tmp;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
+	while (current)
+	{
+		tmp = current;
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			/* Swap nodes */
+			listint_t *prev = tmp->prev;
+			listint_t *next = tmp->next;
 
-    while (current)
-    {
-        temp = current;
-        while (temp->prev && temp->n < temp->prev->n)
-        {
-            swap_nodes(list, temp->prev, temp);
-            print_list(*list);
-        }
-        current = current->next;
-    }
+			if (prev->prev)
+				prev->prev->next = tmp;
+
+			tmp->prev = prev->prev;
+			tmp->next = prev;
+			prev->prev = tmp;
+			prev->next = next;
+
+			if (next)
+				next->prev = prev;
+
+			if (!tmp->prev)
+				*list = tmp;
+
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
+
